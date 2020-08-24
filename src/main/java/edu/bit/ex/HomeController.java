fcef3430f81b0ab368498fccbfdcc67d2a7940a0
DESKTOP-TKMG2RN\bit <rksh333@naver.com>
2020-08-24 09:55:17 +0900

    version3

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-//version-2
+//version-3
 @Controller
 public class HomeController {
 	
